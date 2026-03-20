335f9b266c619af065643badcf8e76f69135c32f
asuka171 <1716145394@qq.com>
2026-03-20 23:44:37 +0800

    [fix] delete hello

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello world!" << std::endl;
     std::cout << "user2 add" << std::endl;
+    std::cout << "user1 add" << std::endl;
     return 0;
 }
